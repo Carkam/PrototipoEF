723224047d2c08af123cd700d43d51e5da5b096b
Carkam <caanco@hotmail.es>
2020-11-24 08:43:22 -0600

    Contenedor mdi con todas las opciones

---
+++ EjecutableExamenFinal/EjecutableExamenFinal/Form1.cs
@@ -0,0 +1,51 @@
+﻿using CapaVistaSeguridad;
+using CapaVista;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjecutableExamenFinal
+{
+    public partial class Form1 : Form
+    {
+        clsFuncionesSeguridad seguridad = new clsFuncionesSeguridad();
+        clsVistaBitacora bit = new clsVistaBitacora();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            frmLogin frm = new frmLogin();
+            if (frm.ShowDialog() == DialogResult.OK)
+            {
+                txtUsuario.Text = frm.usuario();
+            }
+        }
+
+        private void bancosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (seguridad.PermisosAcceso("1302", txtUsuario.Text) == 1)
+            {
+                bit.user(txtUsuario.Text);
+                bit.insert("Ingreso a la aplicacion Cuenta Bancaria", 1302);
+                MantenimientoBanco banco = new MantenimientoBanco(txtUsuario.Text, this);
+                banco.MdiParent = this;
+                banco.Show();
+            }
+            else
+            {
+                bit.user(txtUsuario.Text);
+                bit.insert("Trato de ingresar a la aplicacion Bancos", 1305);
+                MessageBox.Show("El Usuario No Cuenta Con Permisos De Acceso A La Aplicación");
+            }
+        }
+    }
+}
